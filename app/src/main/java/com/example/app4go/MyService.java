025c3c6f4909d7a2cae7a3a8888603debf470504
Benja Netan <netanyahubenjamin70@gmail.com>
2020-08-18 11:24:37 +0300

    Service to play Music in the background

---
+++ app/src/main/java/com/example/app4go/MyService.java
@@ -0,0 +1,36 @@
+package com.example.app4go;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class MyService extends Service {
+    MediaPlayer myplayer;
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "Service Created", Toast.LENGTH_SHORT).show();
+
+        myplayer = MediaPlayer.create(this, R.raw.mus);
+        myplayer.setLooping(false);//set looping
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        myplayer.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
+        myplayer.stop();
+    }
+}
---
+++ app/src/main/java/com/example/app4go/ServicesTwo.java
@@ -0,0 +1,14 @@
+package com.example.app4go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServicesTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_services_two);
+    }
+}
